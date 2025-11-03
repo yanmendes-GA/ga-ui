24bc52b2e86391848f3a729b2eb9e006bc72cccd
yanmendes <yan.mendes@grupoacelerador.com.br>
2025-11-03 09:57:48 -0300

    Corrigindo layout do template padrão

--- src/components/templates/DashboardTemplate/DashboardTemplate.styles.ts
+++ src/components/templates/DashboardTemplate/DashboardTemplate.styles.ts
@@ -2,11 +2,11 @@ import { cn } from "@/utils/cn"
 
 export const getDashboardTemplateStyles = () => {
   return {
-    wrapper: cn("flex h-screen w-full bg-dark text-foreground"),
+    wrapper: cn("flex h-screen w-full bg-dark"),
 
     main: cn("flex h-full flex-1 flex-col overflow-y-auto"),
 
-    sidebar: cn("h-full w-fit max-w-[280px] min-w-[80px] bg-dark-500"),
+    sidebar: cn("h-full w-fit max-w-[280px] min-w-[80px]"),
 
     toolbar: cn("w-full flex-shrink-0 p-5"),
 
